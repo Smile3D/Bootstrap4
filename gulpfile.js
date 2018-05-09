bb288decf8c8aafcba3d7f8116dfb44bf244a994
Aleksandrov Sergey <Aleks140988@gmail.com>
2018-05-10 00:08:05 +0300

    changes sass folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var syntax          = 'sass'; // Syntax: sass or scss;
+
 var gulp        	= require('gulp'),
     sass        	= require('gulp-sass'),
     browserSync 	= require('browser-sync'),
@@ -5,13 +7,24 @@ var gulp        	= require('gulp'),
     imagemin    	= require('gulp-imagemin'),
     pngquant        = require('imagemin-pngquant'),
     cache       	= require('gulp-cache'),
-    autoprefixer 	= require('gulp-autoprefixer');
-    gcmq            = require('gulp-group-css-media-queries');
+    autoprefixer 	= require('gulp-autoprefixer'),
+    gcmq            = require('gulp-group-css-media-queries'),
+    plumber         = require('gulp-plumber'),
+    gutil           = require('gulp-util'),
+    sourcemaps      = require('gulp-sourcemaps');
 
-gulp.task('sass', function() {
-	return gulp.src('scss/**/*.scss')
+gulp.task('styles', function() {
+	return gulp.src(''+syntax+'/**/*.'+syntax+'')
+	.pipe(plumber(function(error) {
+	  gutil.log(gutil.colors.bold.red(error.message));
+	  gutil.beep();
+	  this.emit('end');
+	 }))
+	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer(['last 15 version', '>1%', 'ie 8', 'ie 7'], {cascade: true}))
+	.pipe(sourcemaps.write('./'))
+	.pipe(plumber.stop())
 	.pipe(gulp.dest('css'))
 	.pipe(browserSync.reload({stream: true}))
 });
@@ -19,7 +32,7 @@ gulp.task('sass', function() {
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: '../',
+			baseDir: './',
 			index: "index.html",
             directory: true
 		},
@@ -27,7 +40,7 @@ gulp.task('browser-sync', function() {
 	});
 });
 
-gulp.task('build', ['clean','img', 'sass', 'gcmq'], function() {
+gulp.task('build', ['clean','img', 'styles', 'gcmq'], function() {
 	var buildCSS = gulp.src([
 		'css/main.css'
 	])
@@ -74,8 +87,8 @@ gulp.task('gcmq', function () {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch', ['browser-sync', 'sass'] , function() {
-	gulp.watch('scss/**/*.scss', ['sass']);
+gulp.task('watch', ['browser-sync', 'styles'] , function() {
+	gulp.watch(''+syntax+'/**/*.'+syntax+'', ['styles']);
 	gulp.watch('*.html', browserSync.reload);
 	gulp.watch('**/*.css', browserSync.reload);
 	gulp.watch('js/**/*.js', browserSync.reload);
